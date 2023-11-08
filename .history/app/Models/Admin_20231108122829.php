3c38512858ced8a9d90fd656909d9b577495622d
Fuki-san <ff1028mh@gmail.com>
2023-11-08 13:09:33 +0900

    add multilogin

---
+++ .history/app/Models/Admin_20231108122829.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\A
+
+class Admin extends Authenticatable
+{
+    use HasFactory;
+
+}
---
+++ .history/database/factories/AosukiFactory_20231108082751.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class AosukiFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        $faker = \Faker\Factory::create('ja_JP');
+
+        return [
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail,
+            'email_verified_at' => $faker->time(),
+            'password' => $faker->password(),
+            'two_factor_secret' => $faker->password(),
+            'two_factor_recovery_code' => $faker->password(),
+        ];
+    }
+}
